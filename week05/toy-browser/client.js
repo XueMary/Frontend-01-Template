dd4ff836526553441c67507ee87c3e6d2e7d8c1f
薛纪洪 <632565970@qq.com>
2020-05-13 14:42:26 +0800

    解析 response

--- week05/toy-browser/client.js
+++ week05/toy-browser/client.js
@@ -16,7 +16,7 @@ class Request {
 
     if (this.headers["Content-Type"] === "application/x-www-form-urlencoded") {
       this.bodyText = Object.keys(this.body).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(this.body[key])}`).join("&")
-    } else if(this.headers["Content-Type"] === "application/json") {
+    } else if (this.headers["Content-Type"] === "application/json") {
       this.bodyText = JSON.stringify(this.body)
     }
 
@@ -30,11 +30,11 @@ ${Object.keys(this.headers).map(key => `${key}: ${this.headers[key]}`).join("\r\
 ${this.bodyText}`
   }
 
-  send(client){
-    if(client){
+  send(client) {
+    if (client) {
       client.write(this.toString())
-    }else{ 
-
+    } else {
+      const responseParser = new ResponseParser()
       return new Promise((resolve, reject) => {
         client = net.createConnection({
           host: this.host,
@@ -43,7 +43,10 @@ ${this.bodyText}`
           client.write(this.toString())
         });
         client.on('data', (data) => {
-          resolve(data.toString());
+          responseParser.receive(data.toString())
+          console.log(responseParser.statusLine)
+          console.log(responseParser.headers)
+          // resolve(data.toString());
           client.end();
         });
         client.on('error', (error) => {
@@ -51,12 +54,85 @@ ${this.bodyText}`
           client.end()
         });
       })
-      
+
     }
   }
 }
 
-void async function() {
+
+class ResponseParser {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0
+    this.WAITING_STATUS_LINE_END = 1
+    this.WAITING_HEADER_NAME = 2
+    this.WAITING_HEADER_SPACE = 3
+    this.WAITING_HEADER_VALUE = 4
+    this.WAITING_HEADER_LINE_END = 5
+    this.WAITING_HEADER_BLOCK_END = 6
+    this.WAITING_BODY = 7
+
+    this.current = this.WAITING_STATUS_LINE
+    this.statusLine = ''
+    this.headers = {}
+    this.headerName = ''
+    this.headerValue = ''
+  }
+
+  receive(string) {
+    console.log(string,1111)
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i))
+    }
+  }
+
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END
+      } else {
+        this.statusLine += char
+      }
+    }
+    else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WAITING_HEADER_SPACE
+      } if (char === '\r') {
+        this.current = this.WAITING_BODY
+      } else {
+        this.headerName += char
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WAITING_HEADER_VALUE
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END
+        this.headers[this.headerName] = this.headerValue
+        this.headerName = ""
+        this.headerValue = ""
+      } else {
+        this.headerValue += char
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME
+      }
+    }
+  }
+}
+
+
+
+void async function () {
   const request = new Request({
     method: 'POST',
     host: "127.0.0.1",
